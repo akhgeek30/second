5c02d0aa14906a2d7f308a5a08960c412e7918b5
akhgeek30 <kingakah.biswas521@gmail.com>
2018-09-05 16:25:23 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+import './node_modules/bootstrap/scss/bootstrap.scss';
+import './scss/index.scss';
+
+import './node_modules/jquery/dist/jquery'
+import './node_modules/popper.js/dist/umd/popper';
+import 'bootstrap';
